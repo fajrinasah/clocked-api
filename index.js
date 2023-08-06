7a738741396c0ba0f26f9102c4987aac489bddb3
fajrinasah <fajrinasah@gmail.com>
2023-08-06 20:03:26 +0700

    INIT: basic structure in entry point

---
+++ index.js
@@ -0,0 +1,57 @@
+/*-------------------------------------------------------*/
+// IMPORT FROM DEPENDENCIES
+/*-------------------------------------------------------*/
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import chalk from "chalk";
+
+/*-------------------------------------------------------*/
+// IMPORT FROM LOCAL (SRC)
+/*-------------------------------------------------------*/
+import * as helpers from "./src/helpers/index.js";
+import * as middlewares from "./src/middlewares/index.js";
+
+/*-------------------------------------------------------*/
+// DEPENDENCIES CONFIGURATIONS
+/*-------------------------------------------------------*/
+const app = express();
+dotenv.config();
+
+/*-------------------------------------------------------*/
+// USAGE CONFIGURATIONS
+/*-------------------------------------------------------*/
+app.use(bodyParser.json());
+app.use(cors({ exposedHeaders: "Authorization" }));
+app.use(middlewares.requestLogger);
+
+/*-------------------------------------------------------*/
+// TEST CONNECTION
+/*-------------------------------------------------------*/
+helpers.connectors.redisConnector();
+helpers.connectors.sequelizeConnector();
+
+/*-------------------------------------------------------*/
+// DEFINE ROOT ROUTE
+/*-------------------------------------------------------*/
+app.get("/api", (req, res) => {
+  res.status(200).send("<h1> Connected to clocked API successfully. </h1>");
+});
+
+/*-------------------------------------------------------*/
+// USE ROUTERS
+/*-------------------------------------------------------*/
+
+/*-------------------------------------------------------*/
+// USE ERROR HANDLER
+/*-------------------------------------------------------*/
+app.use(middlewares.errorHandler);
+
+/*-------------------------------------------------------*/
+// LISTEN TO PORT
+/*-------------------------------------------------------*/
+const PORT = process.env.PORT;
+app.listen(PORT, () =>
+  console.log(chalk.bgGreenBright("Server running") + ` on port ${PORT}`)
+);
