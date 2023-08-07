2a0513e030482275070c8e6c2c7cc133bc470c5b
fajrinasah <fajrinasah@gmail.com>
2023-08-08 00:32:58 +0700

    CREATE: validation schemata

---
+++ src/controllers/logs/validationSchemata/clockedInOutValidationSchema.js
@@ -0,0 +1,27 @@
+import * as Yup from "yup";
+
+/*----------------------------------------------------
+CLOCKED IN/OUT VALIDATION SCHEMA
+-----------------------------------------------------*/
+export const clockedInOutValidationSchema = Yup.object({
+  scheduled_date: Yup.string()
+    .required("Scheduled date is required.")
+    .min(
+      10,
+      "Scheduled date's length must be 10 characters (format: YYYY-MM-DD)."
+    )
+    .max(
+      10,
+      "Scheduled date's length must be 10 characters (format: YYYY-MM-DD)."
+    ),
+
+  type: Yup.string()
+    .required("Type is required.")
+    .min(2, "Type's length must be between 2 to 3 characters.")
+    .max(3, "Type's length must be between 2 to 3 characters."),
+
+  time: Yup.string()
+    .required("Time is required.")
+    .min(8, "Time's length must be 8 characters (format hh:mm:ss).")
+    .max(8, "Time's length must be 8 characters (format hh:mm:ss)."),
+});
