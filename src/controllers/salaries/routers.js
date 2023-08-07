c142f77e7c4fe28d2daef10604e69f0645fd3244
fajrinasah <fajrinasah@gmail.com>
2023-08-08 03:52:18 +0700

    CREATE: get one's own payroll reports feature

--- src/controllers/salaries/routers.js
+++ src/controllers/salaries/routers.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { verifyAdmin } from "../../middlewares/index.js";
+import { verifyAdmin, verifyUser } from "../../middlewares/index.js";
 import * as salariesControllers from "./index.js";
 
 const router = Router();
@@ -9,5 +9,10 @@ const router = Router();
 POST
 -------------------------------------------------------------*/
 router.post("", verifyAdmin, salariesControllers.setSalary);
+
+/*------------------------------------------------------------
+GET
+-------------------------------------------------------------*/
+router.get("", verifyUser, salariesControllers.getOwnPayrollReports);
 
 export default router;
